8db72eab6cbaf4a105eb3d904cf54eeded3476a7
HangCRH <1317806770@qq.com>
2026-03-24 22:47:09 +0800

    改为使用datagen生成翻译

--- src/client/java/cn/hangcrh/genshenelements/GenshinImpactElementsDataGenerator.java
+++ src/client/java/cn/hangcrh/genshenelements/GenshinImpactElementsDataGenerator.java
@@ -1,11 +1,15 @@
 package cn.hangcrh.genshenelements;
 
+import cn.hangcrh.genshenelements.datagen.GenshinImpactElementsChineseLangProvider;
+import cn.hangcrh.genshenelements.datagen.GenshinImpactElementsEnglishLangProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class GenshinImpactElementsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(GenshinImpactElementsChineseLangProvider::new);
+		pack.addProvider(GenshinImpactElementsEnglishLangProvider::new);
 	}
 }
